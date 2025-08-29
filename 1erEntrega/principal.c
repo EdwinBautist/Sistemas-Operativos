aef307b0bcb09b912cbf4f601c0e78e9d0f920e7
edwi <2105732g@umich.mx>
2025-08-29 12:20:52 -0600

    Primer commit

---
+++ 1erEntrega/principal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+    char ejecutar[20];
+
+    printf("Ingresa una opción:\n");
+    printf("ejecutar nombre_archivo.asm\n");
+    printf("salir\n");
+    fgets(ejecutar, sizeof(ejecutar))
+    while (opcion != "salir"){
+
+
+
+    printf("Ingresa una opción:\n");
+    printf("ejecutar nombre_archivo.asm\n");
+    printf("salir\n"); 
+    }
+    printf("Saliendo...\n"); 
+    return 0;
+}
---
+++ principal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hola Sistemas Operativos");
+    printf("Prueba Commit");
+    printf("Prueba Elena");
+    return 0;
+}
