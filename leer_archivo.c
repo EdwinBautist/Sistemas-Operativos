cce65bf3dd7806061a9be3493ddcbe286cc3c1f2
edwi <2105732g@umich.mx>
2025-09-03 15:20:05 -0600

    Archivos nuevos

---
+++ leer_archivo.c
@@ -0,0 +1,19 @@
+#include "leer_archivo.h"
+
+void leerArchivo(const char *nombre_archivo){
+
+    char lineas_archivo[10]; // maximo nueve caracteres mas fin de cadena \0
+
+    FILE *abrir_archivo = fopen(nombre_archivo, "r"); // Abrir en modo lectura
+
+    if (abrir_archivo == NULL) { 
+        printf("El archivo %s no existe, o no se pudo abrir\n", nombre_archivo);
+        return;
+    }
+    printf("\nContenido del archivo %s:\n\n", nombre_archivo);
+    while (fgets(lineas_archivo, sizeof(lineas_archivo), abrir_archivo)) {
+        printf("%s", lineas_archivo);
+    }
+printf("\n"); // para que no se vea asi: ADD, Bx$, qguegue el salto de linea y ya 
+    fclose(abrir_archivo);
+} 
